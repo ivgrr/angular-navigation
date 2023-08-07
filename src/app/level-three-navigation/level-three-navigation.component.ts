af0b60acac43b6efa3351df055fc9592e2162d4b
Ivan Herasymov <ivgrrrr@gmail.com>
2023-08-07 16:15:03 +0300

    refactor: refactored level-three-navigation (Issue: FRONT-4)

--- src/app/level-three-navigation/level-three-navigation.component.ts
+++ src/app/level-three-navigation/level-three-navigation.component.ts
@@ -1,18 +1,36 @@
-import { Component, Input } from '@angular/core';
-
-export interface CardItem {
-  title: string;
-  icon?: string;
-  path: string;
-  children?: CardItem[];
-}
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { NavigationService } from '../navigation/navigation.service';
+import { EnhancedNavigationRoute } from '../navigation/navigation.types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-level-three-navigation',
   templateUrl: './level-three-navigation.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LevelThreeNavigationComponent {
-  @Input() children!: CardItem[];
-  @Input() firstChildPath!: string;
-  @Input() firstChildIcon!: string | undefined;
+export class LevelThreeNavigationComponent implements OnInit, OnDestroy {
+  @Input() routePath!: string;
+  private navigationSubscription!: Subscription;
+
+  protected childrenRoutes!: EnhancedNavigationRoute[] | undefined;
+
+  constructor(protected readonly navigationService: NavigationService) {}
+
+  ngOnInit() {
+    this.navigationSubscription = this.navigationService
+      .getRoute(this.routePath)
+      .subscribe((route) => {
+        this.childrenRoutes = route?.children;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.navigationSubscription.unsubscribe();
+  }
 }
