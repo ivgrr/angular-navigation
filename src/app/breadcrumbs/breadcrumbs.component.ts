b40af7b8f5f086be1acc5425cd4a85a7e25b3d4b
Ivan Herasymov <ivgrrrr@gmail.com>
2023-08-07 14:23:52 +0300

    refactor: refactored navigation and breadcrumbs components (Issue: FRONT-2)

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,49 +1,33 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, Signal } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { NavigationService } from '../navigation/navigation.service';
 import { EnhancedNavigationRoute } from '../navigation/navigation.types';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BreadcrumbsComponent implements OnDestroy {
-  breadcrumbs: EnhancedNavigationRoute[] | null = null;
-  routeUrl: string;
-  private subscription$: Subject<void> = new Subject<void>();
+export class BreadcrumbsComponent {
+  protected readonly breadcrumbs!: Signal<
+    EnhancedNavigationRoute[] | null | undefined
+  >;
 
   constructor(
-    private navigationService: NavigationService,
-    private router: Router
+    private readonly navigationService: NavigationService,
+    private readonly router: Router
   ) {
-    this.routeUrl = this.router.url;
-    this.router.events
-      .pipe(takeUntil(this.subscription$))
-      .subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          this.routeUrl = event.url;
-          this.updateBreadcrumbs();
-        }
-      });
-  }
+    const routeIsChanged = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
 
-  private updateBreadcrumbs() {
-    this.navigationService
-      .getBreadcrumbs(this.routeUrl)
-      .pipe(takeUntil(this.subscription$))
-      .subscribe((enhancedData) => {
-        this.breadcrumbs = enhancedData;
-      });
-  }
+    const breadcrumbs = routeIsChanged.pipe(
+      switchMap(() => this.navigationService.getBreadcrumbs(this.router.url))
+    );
 
-  ngOnDestroy() {
-    this.subscription$.next();
-    this.subscription$.complete();
-    if (this.subscription$) {
-      this.subscription$.unsubscribe();
-    }
+    this.breadcrumbs = toSignal(breadcrumbs);
   }
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,33 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavigationService } from './navigation.service';
-import { NavigationRoute } from './navigation.types';
-import { Subscription } from 'rxjs';
-import { Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavigationComponent implements OnInit, OnDestroy {
-  routes: NavigationRoute[] = [];
-  private navigation$!: Subscription;
+export class NavigationComponent {
+  protected readonly routes = toSignal(this.navigationService.getRoutes());
 
-  constructor(
-    private navigationService: NavigationService,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    this.navigation$ = this.navigationService.getRoutes().subscribe((route) => {
-      this.routes = route;
-    });
-  }
-
-  ngOnDestroy() {
-    this.navigation$.unsubscribe();
-  }
-
-  isChildActive(childRoute: NavigationRoute): boolean {
-    return this.router.isActive(childRoute.path, false);
-  }
+  constructor(private readonly navigationService: NavigationService) {}
 }
