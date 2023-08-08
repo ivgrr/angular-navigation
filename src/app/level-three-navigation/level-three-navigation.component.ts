f8246b5ade6e1c77fd47411c9848f0b0fbd2cd10
Ivan Herasymov <ivgrrrr@gmail.com>
2023-08-08 18:03:32 +0300

    chore: added async pipe and delete unsubscribe (Issue: FRONT-4)

--- src/app/level-three-navigation/level-three-navigation.component.ts
+++ src/app/level-three-navigation/level-three-navigation.component.ts
@@ -2,35 +2,26 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnDestroy,
   OnInit,
 } from '@angular/core';
 import { NavigationService } from '../navigation/navigation.service';
 import { EnhancedNavigationRoute } from '../navigation/navigation.types';
-import { Subscription } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-level-three-navigation',
   templateUrl: './level-three-navigation.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LevelThreeNavigationComponent implements OnInit, OnDestroy {
+export class LevelThreeNavigationComponent implements OnInit {
   @Input() routePath!: string;
-  private navigationSubscription!: Subscription;
+  protected childrenRoutes!: Observable<EnhancedNavigationRoute[] | undefined>;
 
-  protected childrenRoutes!: EnhancedNavigationRoute[] | undefined;
-
-  constructor(protected readonly navigationService: NavigationService) {}
+  constructor(private readonly navigationService: NavigationService) {}
 
   ngOnInit() {
-    this.navigationSubscription = this.navigationService
+    this.childrenRoutes = this.navigationService
       .getRoute(this.routePath)
-      .subscribe((route) => {
-        this.childrenRoutes = route?.children;
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.navigationSubscription.unsubscribe();
+      .pipe(map((route) => route?.children));
   }
 }
